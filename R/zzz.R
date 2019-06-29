88ec96c7b0dfb7c1898c6356ae813f0e560fae50
Sacha Epskamp <mail@sachaepskamp.com>
2019-06-30 00:00:59 +0200

    No longer supported message

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("The lvnet package is deprecated and will no longer be supported. Most functionality has been moved to the psychonetrics package (psychonetrics.org).")
+}
